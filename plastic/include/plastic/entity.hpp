2f265f546fbed2390590347fb2ae0889ac10fa33
ajost1 <aidanpj18@gmail.com>
2024-12-30 14:21:51 -0500

    start plastic prototype

--- plastic/include/plastic/entity.hpp
+++ plastic/include/plastic/entity.hpp
@@ -4,7 +4,19 @@
 
 #ifndef PLASTIC_ENTITY_HPP
 #define PLASTIC_ENTITY_HPP
+#include <memory>
 
+using EntityId = int;
+struct Entity {
 
+    virtual ~Entity() = default;
+
+    virtual EntityId entity_id() const = 0;
+    virtual std::shared_ptr<Entity> downgrade() const = 0;
+    virtual std::shared_ptr<Entity> upgrade() const = 0;
+};
+
+using EntityPtr = std::shared_ptr<Entity>;
+using WeakEntityPtr = std::weak_ptr<Entity>;
 
 #endif //PLASTIC_ENTITY_HPP
