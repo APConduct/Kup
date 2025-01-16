8b4a217e184a9e43cba5c6b28b2d821bd821d277
ajost1 <aidanpj18@gmail.com>
2025-01-16 17:28:59 -0500

    update point.hpp

--- libs/plastic/include/plastic/point.hpp
+++ libs/plastic/include/plastic/point.hpp
@@ -6,21 +6,49 @@
 #define POINT_HPP
 
 #include <raylib.h>
+#include <string>
 
 namespace plastic
 {
     template <typename T>
     struct Point {
+        static_assert(std::is_arithmetic_v<T>, "Point must be of an arithmetic type");
         T x, y;
         explicit Point(const T x=0, const T y=0) : x(x), y(y) {}
     };
 
+    template <>
     struct Point<float> {
         float x, y;
         explicit Point(const float x=0, const float y=0) : x(x), y(y) {}
-        explicit Point(const Vector2& vec) : x(vec.x), y(vec.y) {}
-        explicit Point(const Vector2&& vec) : x(vec.x), y(vec.y) {}
+        explicit Point(const Vector2 vec) : x(vec.x), y(vec.y) {}
         [[nodiscard]] Vector2 to_raylib() const { return {x, y}; }
+    };
+
+    // Specialization for cursor position within text
+    template<>
+    struct Point<size_t> {
+        size_t x, y, total;
+        explicit Point(const size_t x, const size_t y, const size_t total) : x(x), y(y), total(total) {}
+        // Construct a Point from a string and a cursor position in the string
+        explicit Point(const std::string& text, const size_t cursor) : x(0), y(0), total(0) {
+            for (size_t i = 0; i < cursor; i++) {
+                if (text[i] == '\n') {
+                    y++;
+                    x = 0;
+                }else {
+                    x++;
+                }
+            }
+            total = cursor;
+        }
+        // Construct a Point from a string and a position x and y
+        explicit Point(const size_t x, const size_t y, const std::string& text) : x(x), y(y), total(0) {
+            for (size_t i = 0; i < y; i++) {
+                total += text.find('\n', total);
+            }
+            total += x;
+        }
     };
 }
 
